7017ecea0d02fcd0011cdb790bf93b1e58370a9a
scelios <scelios@outlook.fr>
2023-03-24 10:40:29 +0100

    Merge I think

--- src/main.c
+++ src/main.c
@@ -6,12 +6,31 @@
 /*   By: beaudibe <beaudibe@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 15:48:26 by beaudibe          #+#    #+#             */
-/*   Updated: 2023/03/24 10:33:02 by beaudibe         ###   ########.fr       */
+/*   Updated: 2023/03/24 10:39:51 by beaudibe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+
+char *ft_remove_n(char *str)
+{
+	if (str[ft_strlen(str) - 1] == '\n')
+		str[ft_strlen(str) - 1] = '\0';
+	return (str);
+}
+
+void	ft_free_global()
+{
+	int i;
+
+	i = -1;
+	while (g_global.environ[++i])
+	{
+		free(g_global.environ[i]);
+	}
+	free(g_global.environ);
+}
 
 int main(int argc, char *argv[], char *envp[])
 {
@@ -35,5 +54,6 @@ int main(int argc, char *argv[], char *envp[])
 	signal(SIGQUIT, SIG_IGN);
 	signal(SIGTSTP, SIG_IGN);
 	char *argv = readline()*/
+	ft_free_global();
 	return (0);
 }
